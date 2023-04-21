dbbb84d3b034f0b70058a5e27f6fc21faf1553b9
phiy <phiy@itu.dk>
2023-04-21 13:04:12 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import HelloWorld from "./HelloWorld/HelloWorld"
+
+const App = () => {
+    
+    return (
+        <div className='container'>
+            <HelloWorld />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import './HelloWorld.css'
+
+const HelloWorld = () => {
+    
+    return (
+        <div>
+            <h1>UX Exam 2023</h1>
+        </div>
+    )
+}
+
+export default HelloWorld
