7b1821a53aa5863d8cd1e6ff2d5f77837486a749
ptso <ptso@itu.dk>
2023-05-18 17:14:19 +0200

    Implementet University Selection + Changed Username to Email along with regex

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,11 +7,13 @@ function LoginForm() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [university, setUniversity] = useState('Select University');
     const [errorMessage, setErrorMessage] = useState('');
+    const regex = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
-        if (event.target.value.length > 5) {
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+        if (regex.test(email)) {
             setErrorMessage('');
         }
     }
@@ -27,10 +29,17 @@ function LoginForm() {
         setConfirmPassword(event.target.value);
     }
 
+    const handleUniversityChange = (event) => {
+        setUniversity(event.target.value);
+        if (!university.includes('Select')) {
+
+        }
+    }
+
     const handleLogin = (event) => { //Login Button Pressed
         event.preventDefault();
-        if (username.length <= 5) {
-            setErrorMessage('Username must be at least 6 characters in length');
+        if (!regex.test(email)) {
+            setErrorMessage('Not an Email');
             return;
         }
         if (password.length < 8) {
@@ -39,13 +48,13 @@ function LoginForm() {
         }
         // TODO: AJAX/Fetch login
         // For now just set an error message
-        setErrorMessage('Invalid username/password combination');
+        setErrorMessage('Invalid email/password combination');
     };
 
     const handleSignup = (event) => { //Sign Up Button Pressed
         event.preventDefault();
-        if (username.length <= 5) {
-            setErrorMessage('Username must be at least 6 characters in length');
+        if (!regex.test(email)) {
+            setErrorMessage('Not an Email');
             return;
         }
         if (password.length < 8) {
@@ -53,7 +62,11 @@ function LoginForm() {
             return;
         }
         if (password !== confirmPassword) {
-            setErrorMessage('Passwords do not match')
+            setErrorMessage('Passwords do not match');
+            return;
+        }
+        if (university.includes('Select University')) {
+            setErrorMessage('Please choose a University');
             return;
         }
         // TODO: AJAX/Fetch signup operation here.
@@ -71,11 +84,11 @@ function LoginForm() {
                     <div className="form-input-group">
                         <input
                             type="text"
-                            className={`form-input ${errorMessage.includes('Username') ? 'form-input-error' : ''}`}
+                            className={`form-input ${errorMessage.includes('Email') ? 'form-input-error' : ''}`}
                             autoFocus
-                            placeholder="Username"
-                            value={username}
-                            onChange={handleUsernameChange}
+                            placeholder="Email"
+                            value={email}
+                            onChange={handleEmailChange}
                         />
                         <div className="form-input-error-message"></div>
                     </div>
@@ -101,6 +114,43 @@ function LoginForm() {
                             Don't have an account? Create account
                         </a>
                     </p>
+                    <p className="form-text">
+                        <a href="#" className="form-link" onClick={() => {
+                            setIsLoginFormActive(false);
+                            setIsGuestFormActive(true);
+                        }}
+                        >
+                            Continue as guest
+                        </a>
+                    </p>
+                </form>
+            ) : isGuestFormActive ? (
+                <form className="form" id="Guest" onSubmit={handleGuestLogin}>
+                    <h1 className="form-title">Guest Login</h1>
+                    <div className="form-message form-message-error">{errorMessage}</div>
+                    <div className="form-input-group">
+                        <input
+                            type="text"
+                            className={`form-input ${errorMessage.includes('GuestID') ? 'form-input-error' : ''}`}
+                            autoFocus
+                            placeholder="ID"
+                            value={guestID}
+                            onChange={handleGuestIDChange}
+                        />
+                        <div className="form-input-error-message"></div>
+                    </div>
+                    <button className="form-button" type="submit">Log in</button>
+                    <p className="form-text">
+                        <a
+                            className="form-link"
+                            onClick={() => {
+                                setIsLoginFormActive(true);
+                                setIsGuestFormActive(false);
+                        }}
+                        >
+                            Already have an account? Sign in
+                        </a>
+                    </p>
                 </form>
             ) : (
                 <form className="form" id="SignUp" onSubmit={handleSignup}>
@@ -109,11 +159,11 @@ function LoginForm() {
                     <div className="form-input-group">
                         <input
                             type="text"
-                            className={`form-input ${errorMessage ? 'form-input-error' : ''}`}
+                            className={`form-input ${errorMessage.includes('Email') ? 'form-input-error' : ''}`}
                             autoFocus
-                            placeholder="Username"
-                            value={username}
-                            onChange={handleUsernameChange}
+                            placeholder="Email"
+                            value={email}
+                            onChange={handleEmailChange}
                         />
                         <div className="form-input-error-message"></div>
                     </div>
@@ -135,6 +185,20 @@ function LoginForm() {
                             value={confirmPassword}
                             onChange={handleConfirmPasswordChange}
                         />
+                        <div className="form-input-error-message"></div>
+                    </div>
+                    <div className="form-input-group">
+                        <select
+                            className={`form-input ${errorMessage.includes('University') ? 'form-input-error' : ''}`}
+                            value={university}
+                            onChange={handleUniversityChange}
+                        >
+                            //TODO Interact with data so show universities:
+                            <option value="">Select University</option>
+                            <option value="university1">University 1</option>
+                            <option value="university2">University 2</option>
+                            <option value="university3">University 3</option>
+                        </select>
                         <div className="form-input-error-message"></div>
                     </div>
                     <button className="form-button" type="submit">Sign up</button>
